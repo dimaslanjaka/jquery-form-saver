15cbc78cb9ba832a1c4614003c181eb9a500f134
dimaslanjaka <dimaslanjaka@gmail.com>
2021-05-31 14:50:22 +0700

    Scrutinize JSOnly
    Signed-off-by: dimaslanjaka <dimaslanjaka@gmail.com>

--- src/js/_conf.ts
+++ src/js/_conf.ts
@@ -1,28 +1,26 @@
-if (typeof makeid == "undefined") {
-    /**
-     * unique id generator
-     * @param length digit number string
-     * @returns random string
-     */
-    var makeid = function (length: number) {
-        var result = "";
-        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
+/**
+ * unique id generator
+ * @param length digit number string
+ * @returns random string
+ */
+function makeid(length: number) {
+    let result = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
 
-        return result;
-    };
+    return result;
 }
 
 /**
  * Local Storage key
  */
-var storageKey: String = location.pathname.replace(/\/$/s, "") + "/formField";
+const storageKey: String = location.pathname.replace(/\/$/s, "") + "/formField";
 
-var formFieldBuild: object | Array<any>;
-var formSaved = localStorage.getItem(storageKey.toString());
+let formFieldBuild: object | Array<any>;
+const formSaved = localStorage.getItem(storageKey.toString());
 if (!formSaved) {
     formFieldBuild = [];
 } else {
@@ -32,16 +30,17 @@ if (!formSaved) {
 /**
  * Element Indexer
  */
-var formField = formFieldBuild;
+const formField = formFieldBuild;
 
-var uniqueid = makeid(5);
+const uniqueid = makeid(5);
 
 /**
  * check if running in browser
  */
-var isBrowser = new Function("try {return this===window;}catch(e){ return false;}");
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const isBrowser = new Function("try {return this===window;}catch(e){ return false;}");
 
 /**
  * Element Counter
  */
-var Count = -1;
+const Count = -1;
