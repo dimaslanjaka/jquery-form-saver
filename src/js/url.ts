cee0a210d0bdea2908e8e551bfa8de3a15fc6dbe
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-14 21:33:51 +0700

    get pathname directly

--- src/js/url.ts
+++ src/js/url.ts
@@ -1,5 +1,3 @@
 export const isIframe = window.self !== window.top;
 export const url = new URL(isIframe ? document.referrer : document.location.href);
-export function getPathname() {
-  return url.pathname;
-}
+export const pathname = url.pathname;
